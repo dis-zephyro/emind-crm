3fb6ec7f3c60590aea82bd166ac2968aac5fd8c2
dis-zephyro <zephyro@yandex.ru>
2015-08-17 04:27:38 +0300

    v1

--- js/main.js
+++ js/main.js
@@ -3,3 +3,42 @@ $('.btn-yellow').click(function(){
     $('.reply-hidden').show();
     $('.btn-yellow').hide();
 });
+
+
+$.validate({
+    form : '.form'
+});
+
+
+$(document).ready(function() {
+
+    // Анимация
+    var Android = navigator.userAgent.search(/Android/i);
+    var iPhone = navigator.userAgent.search(/iPhone/i);
+    var iPad = navigator.userAgent.search(/iPad/i);
+    if(Android != -1 || iPhone != -1 || iPad != -1) {
+
+    } else {
+
+
+        $(".scroll").each(function () { // анимация по скролу(используйте этот) достаточно добавить анимируемому блоку класс 'scroll' а css анимацию писать так: '.animated.класс_блока'
+            var block = $(this);
+            $(window).scroll(function() {
+                var top = block.offset().top;
+                var bottom = block.height()+top;
+                top = top - $(window).height();
+                var scroll_top = $(this).scrollTop();
+                if ((scroll_top > top) && (scroll_top < bottom)) {
+                    if (!block.hasClass("animated")) {
+                        block.addClass("animated");
+                    }
+                } else {
+                    block.removeClass("animated");
+                }
+            });
+        });
+        $('head').append('<link rel="stylesheet" type="text/css" href="css/animate.css" />'); //подключение файла animation.css если не мобильник
+    }
+
+
+});
